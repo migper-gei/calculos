9814be3cd7313338a3a3c4f8d92428b47d621bcc
Miguel <migarper@gmail.com>
2024-01-20 19:41:08 +0000

    acrescentar código para testes

--- Program.cs
+++ Program.cs
@@ -15,6 +15,13 @@ namespace Calculos
             Console.WriteLine($"4+2={aritmetica.somar(4,2)}");
             Console.WriteLine($"4-2={aritmetica.subtrair(4, 2)}");
 
+            double t1 =conversoes.ConverterTemperatura(conversoes.ConversaoTemperatura.CelsiusFahrenheit, 36);
+            double t2 = conversoes.ConverterTemperatura(conversoes.ConversaoTemperatura.FahrenheitCelsius, 100);
+
+            Console.WriteLine($"36º Celsius = {t1}º Fahrenheit");
+            Console.WriteLine($"100º Fahrenheit = {t2}º Celsius");
+
+
             Console.ReadKey(); 
         }
     }
