74c0048c4d5797ef57110064330f02d92196d9d9
Miguel <migarper@gmail.com>
2024-01-20 18:44:23 +0000

    adicionar método subtrair

--- aritmetica.cs
+++ aritmetica.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace Calculos
 {
+    //implementa operações aritmécias
     internal class aritmetica
     {
+        public int somar(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int subtrair(int x, int y)
+        {
+            return x - y;
+        }
     }
 }
